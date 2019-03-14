ec3d25de2bae73f2813255750fd0bbc898f9fc34
Max Fleischman <maxecf@gmail.com>
2019-03-14 10:08:34 -0700

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+state = {
+    title: '',
+    body: ''
+};
+
+    componentDidMount() {
+        fetch
+        ('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => response.json())
+        .then(data => this.setState({
+            title: data.title,
+            body: data.body
+        })
+        );
+    }
+    // componentWillMount() {
+    //     console.log('component will mount...');
+    // }
+    // componentDidUpdate() {
+    //     console.log('component did update...');
+    // }
+    // componentWillUpdate() {
+    //     console.log('component will update...');
+    // }
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     console.log('component receives new props..');
+    // }
+
+    // static getDerivedStateFromProps(nextPorps, prevState) {
+    //     return null;
+    // }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log('snapshot before update');
+    // }
+
+  render() {
+      const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
